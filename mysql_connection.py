fd0c36ca4f246342bc7102e913cfa1e3993b2e59
soej24 <soej24@naver.com>
2022-06-28 17:59:44 +0900

    add

--- mysql_connection.py
+++ mysql_connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_connection() :
     connection = mysql.connector.connect(
             host = 'yhdb.c8nbdutl9vtz.ap-northeast-2.rds.amazonaws.com',
-            database = 'memo_db',
+            database = 'recipe_db',
             user = 'soej',
             password = 'soej1234')
     
